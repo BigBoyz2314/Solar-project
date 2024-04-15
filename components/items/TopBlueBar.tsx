b9291fcbce9e020115b0e0fc123d01fb57e814dc
BigBoyz2314 <zazimran@gmail.com>
2024-04-16 01:17:31 +0500

    first item

---
+++ components/items/TopBlueBar.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Marquee from 'react-fast-marquee'
+
+const TopBlueBar = () => {
+  return (
+    <Marquee
+          className="text-white text-sm leading-6 mb-0 scroller py-3.5 max-md:max-w-full"
+        >
+          •••  
+          Partnerschaften mit renommierten Herstellern und Zulieferern . Fachkundige
+          Beratung durch erfahrene Mitarbeiter - kostenlos und unverbindlich . Große
+          Auswahl an verschiedenen Solaranlagen-Modellen 
+          •••  
+          Partnerschaften mit renommierten Herstellern und Zulieferern . Fachkundige
+          Beratung durch erfahrene Mitarbeiter - kostenlos und unverbindlich . Große
+          Auswahl an verschiedenen Solaranlagen-Modellen 
+    </Marquee>
+  )
+}
+
+export default TopBlueBar
